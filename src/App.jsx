232230a8a908e06e9b63f061502808463698b4fc
Abhishek Kumar <abhishekkumar.ece1@bvp.edu.in>
2023-09-13 21:11:49 +0530

    css added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import './App.css'
 import Cypher from "./Cypher-text/Cypher"
 import CypherImage from './Cypher-image/cypherImage'
+import { useState } from 'react'
 function App() {
-
+  const [text, setText] = useState(true);
   return (
     <>
-      {/* <h1 id='head'>Welcome</h1> */}
-      {/* <Cypher /> */}
-      <CypherImage />
-
+      <h1 id='head'>Welcome To Cypher App</h1>
+      <div id='Cypher-div'>
+        <button className={text ? "active" : "cypher"} onClick={() => { setText(true) }} >Text Cypher</button>
+        <button className={text ? "cypher" : "active"} onClick={() => { setText(false) }} >Image Cypher</button>
+      </div>
+      {text ? <Cypher /> : <CypherImage />}
     </>
   )
 }
